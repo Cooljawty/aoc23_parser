449b195a33032641d7de5d5570c888ce8a60aa46
Jacari Harper <jacariguyman2@gmail.com>
2023-12-09 14:18:38 -0800

    Added test cases for day one

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use reqwest::header::{ USER_AGENT, COOKIE};
+use reqwest::header::{ USER_AGENT, COOKIE, CONNECTION };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let auth = "53616c7465645f5f64963b27dc1a96e3b498e57c182fdeeaffe976345070fc3d0f22c88c1d91459933897ad530ab45683a294585bdea24b9a034f97ee4c8d46b";
@@ -38,6 +38,7 @@ fn pop_number(value: &mut String) -> Option<usize> {
 
         //Try getting numerics first
         let digits: Vec<&str> = substr.matches(char::is_numeric).collect();
+        println!("{:?}", digits);
         if !digits.is_empty() { return Some(digits.first()?.parse::<usize>().ok()?) }
 
         for token in &TOKENS {
@@ -58,6 +59,7 @@ fn get_input(src: &str, auth: &str) -> Result<Vec<String>, Box<dyn std::error::E
     let res = reqwest::blocking::Client::new().get(src)
         .header(USER_AGENT, "")
         .header(COOKIE, format!("session={}", auth))
+        .header(CONNECTION, "keep-alive")
         .send()?;
 
     let res_body= res.text()?;
@@ -73,4 +75,23 @@ fn get_input(src: &str, auth: &str) -> Result<Vec<String>, Box<dyn std::error::E
 
     Ok(input)
 
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn day1_part2() {
+        let test_values = vec!(
+            ("onthreethreeboat", 33),
+            ("twoeightwo", 28),
+            ("bxfour3two2sb4twondmfdpsz", 42),
+        );
+
+        for (input, output) in test_values{
+            assert_eq!(parse_input(&input.to_string()), output, 
+            "Given '{input}', expected {output} but got {}", parse_input(&input.to_string()))
+        }
+    }
 }
