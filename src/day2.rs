35eab5635212de1b36ba0e5c7bc5f0af77c6c7a4
Jacari Harper <jacariguyman2@gmail.com>
2023-12-10 16:35:53 -0800

    Pass refrence to game index as parameter

--- src/day2.rs
+++ src/day2.rs
@@ -1,16 +1,24 @@
 use advent_of_code_2023::tokenizer::Token;
 
 pub fn get_answer(input: Vec<String>) -> u32{
-    for line in input {
-        println!("{}", line);
-        parse_input(&line);
-        break;
+    let mut sum = 0;
+    'line: for line in input {
+        let mut index: u32 = 0;
+        for result in parse_input(&line, &mut index).unwrap() {
+            match result {
+                (r, g, b) if r > 12 || g > 13 || b > 14 => { 
+                    continue 'line;
+                },
+                _ => {},
+            };
+        }
+        sum += index;
     }
 
-    return 0;
+    return sum;
 }
 
-fn parse_input(input: &String) -> Option<Vec<(u32, u32, u32)>> {
+fn parse_input(input: &String, index: &mut u32) -> Option<Vec<(u32, u32, u32)>> {
     //Sperate games into matches
     let matches: Vec<&str> = input
         .split(|c: char| { c.is_whitespace() })
@@ -21,11 +29,11 @@ fn parse_input(input: &String) -> Option<Vec<(u32, u32, u32)>> {
         .collect();
     
     //Extract game index
-    let Ok(Token::Count(index)) = matches[1].to_string().parse::<Token>() else {panic!("Missing game index!")};
+    let Ok(Token::Count(game)) = matches[1].to_string().parse::<Token>() else {panic!("Missing game index!")};
+    *index = game;
 
     get_result(matches)
 
-    //Some((index.try_into().ok()?, vec!((0, 0, 0))))
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -38,7 +46,6 @@ fn get_result(input: Vec<&str>) -> Option<Vec<(u32, u32, u32)>> {
             Some(token) => { token_stack.push(token); },
             None => {},
         }
-        //println!("input: {:?}\nstack: {:?}", word, token_stack)
     }
 
     //Evaluate stack
@@ -75,7 +82,6 @@ fn get_result(input: Vec<&str>) -> Option<Vec<(u32, u32, u32)>> {
                 };
 
                 curr_color = Some(new_color);
-                //println!("{:?} {:?}", curr_color, curr_match);
             },
             Token::Count(digit) => count = digit, 
             Token::Seperator(s) => match s {
@@ -99,7 +105,6 @@ fn get_result(input: Vec<&str>) -> Option<Vec<(u32, u32, u32)>> {
             _ => {}, 
         }
     }
-    println!("{:?} ", matches);
     matches.reverse();
     
     Some(matches)
@@ -111,7 +116,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1() {
+    fn parser() {
         let test_values = vec!(
             ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", vec!((4,0,3),(1,2,6),(0,2,0)), true),
             ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", vec!((0,2,1),(1,3,4),(0,1,1)), true),
@@ -121,7 +126,23 @@ mod tests {
         );
 
         for (input, output, _) in test_values{
-            assert_eq!(parse_input(&input.to_string()), Some(output)); 
+            let mut index = 0;
+            assert_eq!(parse_input(&input.to_string(), &mut index), Some(output), "Parser error"); 
         }
+
+
+    }
+
+    #[test]
+    fn part1() {
+        let test_input = vec!(
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        );
+
+        assert_eq!(get_answer(test_input.iter().map(|s| s.to_string()).collect()), 8);
     }
 }
