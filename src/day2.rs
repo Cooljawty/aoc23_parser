cdd448c597a0095e4d7e4f938491544df56f58ae
Jacari Harper <jacariguyman2@gmail.com>
2023-12-12 22:41:04 -0800

    Simplified day 2 token parser

--- src/day2.rs
+++ src/day2.rs
@@ -24,6 +24,7 @@ pub fn get_answer_part_1(input: Vec<String>) -> Result<u32, Box<dyn std::error::
 pub fn get_answer(input: Vec<String>) -> Result<u32, Box<dyn std::error::Error>> {
     let mut sum = 0;
     for line in input {
+        println!("{line}");
         let mut index: u32 = 0;
         let mut color_count = (0,0,0);
         for result in get_result(tokenize(line.as_bytes())?, &mut index) {
@@ -40,74 +41,58 @@ pub fn get_answer(input: Vec<String>) -> Result<u32, Box<dyn std::error::Error>>
     Ok(sum)
 }
 
-#[derive(Debug, PartialEq, Eq)]
-enum Color { Red, Green, Blue }
 fn get_result(mut input: Vec<Token>, index: &mut u32) -> Vec<(u32, u32, u32)> {
     //Evaluate stack
-    let mut count: u32 = 0;
-    let mut curr_color: Option<Color> = None;
-
     let mut matches = Vec::<(u32, u32, u32)>::new();
     let mut curr_match = (0,0,0);
+
+    let mut tokens = Vec::<Token>::new();
     while let Some(token) = input.pop() { 
-        match token {
-            Token::Keyword(color) => {
-                if color != "Game" {
-                    let new_color = match color.as_str() {
-                        "red" => Color::Red,
-                        "green" => Color::Green,
-                        "blue" => Color::Blue,
-                        _ => {panic!("Loose color matching.\nThis should not happen!")}
-                    };
-
-                    match curr_color {
-                        //Reset on repead color token
-                        Some(ref color) if *color == new_color  => {count = 0}
-                        Some(ref color) => { 
-                            match color {
-                                Color::Red => { curr_match.0 = count; },
-                                Color::Green => { curr_match.1 = count; },
-                                Color::Blue => { curr_match.2 = count; },
-                            }
-                        },
-                        None => match new_color {
-                                Color::Red => { curr_match.0 = count; },
-                                Color::Green => { curr_match.1 = count; },
-                                Color::Blue => { curr_match.2 = count; },
-                        },
-                    };
-
-                    curr_color = Some(new_color);
-                } else if color == "Game" {
-                    *index = count;
-                }
+        tokens.push(token);
+        println!("{tokens:?}");
+        match &tokens[..] {
+            [Token::Keyword(color), Token::Count(num)] if color != "Game"=> {
+                println!("{color} is {num}");
+                match color.as_str() {
+                    "red" =>  { curr_match.0 = *num; },
+                    "green" => { curr_match.1 = *num; },
+                    "blue" => { curr_match.2 = *num; },
+                    _ => {panic!("Loose color matching. {color} is not a valid color!")}
+                };
+                
             },
-            Token::Count(digit) => count = digit, 
-            Token::Seperator(s) => match s.as_str() {
-                ";"|":" => { 
-                    match curr_color {
-                        Some(ref color) => { 
-                            match color {
-                                Color::Red => { curr_match.0 = count; },
-                                Color::Green => { curr_match.1 = count; },
-                                Color::Blue => { curr_match.2 = count; },
-                            }
-                        },
-                        None => {},
-                    };
-                    matches.push(curr_match); 
-                    curr_match = (0,0,0); 
-                    count = 0;
+            [Token::Seperator(s)] => {
+                match s.as_str() {
+                    ";" => {
+                        matches.push(curr_match);
+                        println!("result is {curr_match:?}, {matches:?}");
+                        curr_match = (0,0,0);
+                        tokens.clear();
+                    },
+                    "," => {
+                        //Ignore colon for now
+                        tokens.clear();
+                    },
+                    _ => {continue}
                 }
-                _ => {}
             }
-            _ => {}, 
+            [ Token::Seperator(s), Token::Count(num), Token::Keyword(k)] if k == "Game" && s == ":" => {
+                println!("Game index is {num}");
+                matches.push(curr_match);
+                curr_match = (0,0,0);
+                *index = *num;
+            }
+            _ => {continue},
         }
+
+        //Clear stack when rule is met
+        tokens.clear();
     }
+
+    println!("matches: {matches:?}");
     matches.reverse();
     
     matches
-
 }
 
 #[cfg(test)]
@@ -115,7 +100,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1() {
+    fn part1() -> Result<(), Box<dyn std::error::Error>> {
         let test_input = vec!(
             "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
             "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
@@ -123,12 +108,13 @@ mod tests {
             "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
             "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
         );
-        let result = get_answer_part_1(test_input.iter().map(|s| s.to_string()).collect());
+        let result = get_answer_part_1(test_input.iter().map(|s| s.to_string()).collect())?;
         assert_eq!(result, 8, "Expected {:?}, got {:?}", 8, result); 
+        Ok(())
     }
 
     #[test]
-    fn part2() {
+    fn part2() -> Result<(), Box<dyn std::error::Error>> {
         let test_input = vec!(
             "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
             "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
@@ -137,7 +123,8 @@ mod tests {
             "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
         );
 
-        let result = get_answer(test_input.iter().map(|s| s.to_string()).collect());
+        let result = get_answer(test_input.iter().map(|s| s.to_string()).collect())?;
         assert_eq!(result, 2286, "Expected {:?}, got {:?}", 2286, result); 
+        Ok(())
     }
 }
